ef3430f81e0c5888c33b664a346a17634d47c967
Isabela Marin Escobar <ikd.publicidad@gmail.com>
2023-07-13 13:59:26 -0500

    Finish logic of post api

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -1,26 +1,154 @@
-const postModel = request('../models/postModel');
+const { validationResult } = require('express-validator');
+const PostModel = request('../models/postModel');
 const error = new Error();
 
-exports.getAllPosts = (req, res, next) => {
-
+exports.getAllPosts = async (req, res, next) => {
+  try {
+    const allpost = PostModel.find();
+    return res
+          .status(200)
+          .json({
+            items: [allpost],
+            state: true,
+            error: ''
+          })
+  } catch (error) {
+    return res
+          .status(204)
+          .json({
+            items: [],
+            state: false,
+            error: 'No Existing post'
+          })
+  }
 }
 
 exports.getPostByUser = (req, res, next) => {
-
+  const error = validationResult(req);
+  if(!error.isEmpty()){
+    const { id } = req.body;
+    try {
+      const postUser = PostModel.find().where('idUser').equal(id);
+      return res
+            .status(200)
+            .json({
+              items: [postUser],
+              state: true,
+              error: ''
+            })
+    } catch (error) {
+      return res
+            .status(204)
+            .json({
+              items: [],
+              state: false,
+              error: 'User Dont have posts'
+            })
+    }
+  }
 }
 
 exports.createPost = (req, res, next) => {
-
+  const error = validationResult(req);
+  if(!error.isEmpty()){
+    const { idUser, title, description } = req.body;
+    const newPost = new PostModel({
+      idUser: idUser,
+      title: title,
+      description: description
+    });
+    newPost.save()
+      .then((data) => {
+        if(data){
+          return res
+          .status(201)
+          .json({
+            items: [data],
+            state: true,
+            error: ''
+          })
+        }
+      })
+      .catch(err => {
+        return res
+        .status(422)
+        .json({
+          items: [],
+          state: false,
+          error: err
+        })
+      })
+  }
 }
 
-exports.filterPostTitle = (req, res, next) => {
-
+exports.filterPostTitle = async (req, res, next) => {
+  const postUser =  await PostModel.aggregate(
+    [ { $match : { title : req.params.text } } ]
+  );
+  if(postUser){
+    return res
+            .status(200)
+            .json({
+              items: [postUser],
+              state: true,
+              error: ''
+            })
+  }else{
+    return res
+            .status(204)
+            .json({
+              items: [],
+              state: false,
+              error: 'Not Found'
+            })
+  }
 }
 
-exports.filterPostDate = (req, res, next) => {
-
+exports.filterPostDate = async (req, res, next) => {
+  const inputDate = new Date(req.params.date.toISOString());
+  const postUser =  await PostModel.find({
+      'date': { $lte: inputDate }
+  });
+  if(postUser){
+    return res
+            .status(200)
+            .json({
+              items: [postUser],
+              state: true,
+              error: ''
+            })
+  }else{
+    return res
+            .status(204)
+            .json({
+              items: [],
+              state: false,
+              error: 'Not Found'
+            })
+  }
 }
 
-exports.filterPostDateUser = (req, res, next) => {
-
+exports.filterPostDateUser = async (req, res, next) => {
+  const inputDate = new Date(req.params.date.toISOString());
+  const idUser = req.params.id;
+  const postUser =  await PostModel.find({
+      'date': { $lte: inputDate }
+  }).where('idUser').equal(idUser);
+  if(postUser){
+    return res
+            .status(200)
+            .json({
+              items: [postUser],
+              state: true,
+              error: ''
+            })
+  }else{
+    return res
+            .status(204)
+            .json({
+              items: [],
+              state: false,
+              error: 'Not Found'
+            })
+  }
 }
--- app/routes/post.js
+++ app/routes/post.js
@@ -4,10 +4,18 @@ const { body } = require('express-validator');
 const postController = require('../controllers/post_controller');
 
 router.get('/all-posts', postController.getAllPosts);
-router.post('/my-posts', postController.getPostByUser);
-router.post('/create-post', postController.createPost);
+router.post('/my-posts', [
+  body('id').trim().notEmpty().isEmail()
+]
+,postController.getPostByUser);
+router.post('/create-post', [
+  body('idUser').trim().notEmpty().isEmail(),
+  body('title').trim().notEmpty().isEmail(),
+  body('description').trim().notEmpty().isEmail()
+]
+,postController.createPost);
 router.post('/filter-title/:text', postController.filterPostTitle);
 router.post('/filter-date/:date', postController.filterPostDate);
-router.post('/filter-date-user/:date', postController.filterPostDateUser);
+router.post('/filter-date/:date/:id', postController.filterPostDateUser);
 
 module.exports = router;
